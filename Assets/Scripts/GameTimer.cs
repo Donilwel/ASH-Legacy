5157572810006cadc80deb208efce4f82dea6987
Данила <dyumaslov@edu.hse.ru>
2024-04-09 13:20:37 +0300

    release v0.0.1. Create a best time score

--- Assets/Scripts/GameTimer.cs
+++ Assets/Scripts/GameTimer.cs
@@ -3,13 +3,19 @@ using UnityEngine.UI;
 
 public class GameTimer : MonoBehaviour
 {
-    public Text timerText; 
+    public Text timerText;
+    public Text bestTimeText;
+
     private float startTime;
     private bool timerActive = true;
+    private float bestTime;
+
+    private const string BestTimePrefKey = "BestTime";
 
     void Start()
     {
         startTime = Time.time;
+        LoadBestTime();
     }
 
     void Update()
@@ -27,8 +33,15 @@ public class GameTimer : MonoBehaviour
         int minutes = (int)(timeToFormat / 60);
         int seconds = (int)(timeToFormat % 60);
         int milliseconds = (int)((timeToFormat * 1000) % 1000);
-        return string.Format("Время жизни: " + "{0:00}:{1:00}.{2:000}", minutes, seconds, milliseconds);
+        return string.Format("{0:00}:{1:00}.{2:000}", minutes, seconds, milliseconds);
     }
+
+    void LoadBestTime()
+    {
+        bestTime = PlayerPrefs.GetFloat(BestTimePrefKey, float.MaxValue);
+        bestTimeText.text = bestTime != float.MaxValue ? FormatTime(bestTime) : "N/A";
+    }
+
 
     public void StopTimer()
     {
@@ -38,5 +51,14 @@ public class GameTimer : MonoBehaviour
     public void PlayerDied()
     {
         StopTimer();
+
+        float currentTime = Time.time - startTime;
+        if (currentTime > bestTime)
+        {
+            bestTime = currentTime;
+            PlayerPrefs.SetFloat(BestTimePrefKey, bestTime);
+            PlayerPrefs.Save(); 
+            bestTimeText.text = FormatTime(bestTime);
+        }
     }
 }
